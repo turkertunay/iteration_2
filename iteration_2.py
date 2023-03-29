34ebeffaccb225b3af0f3051f738a8edc749d6a6
turkertunay <75833839+turkertunay@users.noreply.github.com>
2023-03-29 23:32:17 +0300

    Add files via upload

---
+++ iteration_2.py
@@ -0,0 +1,17 @@
+word = "distributor"
+myword = iter(word)
+
+print(next(myword))
+print(next(myword))
+print(next(myword))
+print(next(myword))
+print(next(myword))
+print(next(myword))
+print(next(myword))
+print(next(myword))
+print(next(myword))
+print(next(myword))
+print(next(myword))
+
+for x in word:
+    print(x)
